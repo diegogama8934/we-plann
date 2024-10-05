45a851cd856c62e2374ec054898b55e9d97831c1
Diego Martinez <diegogama8934@gmail.com>
2024-10-05 11:37:53 -0600

    add: ugly sign in form

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { Button } from "@/components/basic/Button";
 
 // Onboarding
 export { LoginForm } from "@/components/onboarding/LoginForm";
+export { SignInForm } from "@/components/onboarding/SignInForm";
