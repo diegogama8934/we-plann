83107a3d1d998f95f13b8f8baa4b39e67c66a3e9
Diego Martinez <diegogama8934@gmail.com>
2024-10-05 11:59:58 -0600

    add: content on root directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import { Metadata } from "next";
+import Link from "next/link";
 
-export const metadata:Metadata = {
-  title : "We - plan",
+export const metadata: Metadata = {
+  title: "We plan",
   description: "Platform to plan and manage couple bills and the earnings of each one."
 }
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className=" h-screen flex flex-col pt-32 items-center gap-12">
+      <h1 className="text-4xl font-bold">We Plan</h1>
+      <h2 className="text-center leading-loose text-xl">El espacio donde las parejas pueden planear, administrar y organizar sus finanzas.</h2>
     </div>
   );
 }
