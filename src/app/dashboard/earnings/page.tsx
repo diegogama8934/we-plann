bad6a4fa5011051bce2372888134dd9aa79fce1c
Diego Martinez Garcia <diegogama8934@gmail.com>
2024-12-22 18:53:56 -0600

    ADD: modal component started with a form

--- src/app/dashboard/earnings/page.tsx
+++ src/app/dashboard/earnings/page.tsx
@@ -1,8 +1,26 @@
-import { Button } from "@/ui";
+"use client";
+
+import { Button, Input, InputDate, Modal } from "@/ui";
 import { AiOutlinePlus } from "react-icons/ai";
+import { useState } from "react";
 
 export default function EarningsPage() {
 
+  const [date, setDate] = useState<string>(getToday());
+
+  function onDateChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setDate(e.target.value);
+  }
+
+  function getToday(): string {
+    const date = new Date();
+
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
+
+    return `${year}-${month}-${day}`;
+  }
 
   return (
     <div className="flex flex-col p-12 gap-12">
@@ -22,7 +40,31 @@ export default function EarningsPage() {
         </Button>
 
       </div>
-    
+
+      <Modal
+        title="Agregar ganancia"
+        body={
+          <form className="flex flex-col gap-4">
+            <Input label="Concepto" placeholder="Concepto" />
+            <Input label="Monto" placeholder="Monto" />
+            <Input label="Etiqueta" placeholder="Selecciona una etiqueta..." />
+            <InputDate
+              label="Selecciona una fecha"
+              placeholder="Selecciona una fecha"
+              onValueChange={onDateChange}
+              value={date}
+            />
+            <Input label="Descripción (opcional)" placeholder="Agrega una descripción (opcional)" />
+          </form>
+        }
+        footer={
+          <div className="flex justify-end gap-4 w-full">
+            <Button variant="tertiary">Cancelar</Button>
+            <Button>Guardar</Button>
+          </div>
+        }
+      />
+
     </div>
   );
 }
