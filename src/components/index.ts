0ebaa3bf8349409644366d60b3cb96903b10a6d4
Diego Martinez Garcia <diegogama8934@gmail.com>
2025-09-17 21:11:10 -0600

    REMOVE: Eliminate unused chart exports from components index to simplify the module structure

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,8 +5,5 @@ export { LandingNavbar } from "@/components/onboarding/LandingNavbar";
 export { ActiveLandingLink } from "@/components/onboarding/ActiveLandingLink";
 
 // Platform
-export { ChartsControls } from "@/components/core/ChartsControls";
 export { ActiveNavBarLink } from "@/components/core/ActiveNavBarLink";
-export { ChartsGrid } from "@/components/core/ChartsGrid";
-export { Chart } from "@/components/core/Chart";
 export { HomeQuickActions } from "@/components/core/HomeQuickActions";
